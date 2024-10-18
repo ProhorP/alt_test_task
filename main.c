622e35b19d304fa2c68ba555a1c28090d663b836
prohorp <usoltsev05@mail.ru>
2024-10-18 15:44:50 +0800

    Сделал подсказку при некорректном вводе

--- main.c
+++ main.c
@@ -5,8 +5,12 @@ int main(int argc, char **argv)
 {
     int ret = EXIT_SUCCESS;
 
-    if (argc < 3)
-        ERR_MAIN_EXIT("few arguments\n");
+    if (argc != 3)
+    {
+        printf("example:\ncli_util p10 sisyphus\n");
+        ret = EXIT_FAILURE;
+        goto end;
+    }
 
     if (diff_branch_pack(argv[1], argv[2]) < 0)
         ERR_MAIN_EXIT("diff_branch_pack");
